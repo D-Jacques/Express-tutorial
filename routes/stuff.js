6e2293dfeba5bbf5a1688b6820774edc9418ab17
jacques <jacques.ducroux@gmail.com>
2024-06-17 10:08:11 +0200

    express + mongo tutorial

---
+++ routes/stuff.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const router = express.Router();
+
+router.post('/', auth, multer, stuffCtrl.createThing);
+// router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
+
+module.exports = router
